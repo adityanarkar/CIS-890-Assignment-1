d00022d2fdaf5afb1093e2eddf357ba9da2e68ac
Aditya Narkar <adityanarkar@adityas-mbp.attlocal.net>
2018-01-31 22:41:19 -0600

    RandomFormulaGenerator, README and Zip file of 100 2CNFs

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # CIS-890-Assignment-1
+
+Formula will be generated using following arguments:
+
+-n <N>: Number of non-empty clauses that should occur in a formula
+
+-k <K>: Maximum number of variables that can occur in a formula
+
+-l <L>: Maximum size of each clause
+
+--strict: Every clause should have l literals (optional)
+
+Instructions to generate CNF formula
+
+1. Go to the folder where the groovy file *RandomFormulaGenerator.groovy* is.
+
+2. Enter following command in CLI.
+
+ groovy RandomFormulaGenerator.groovy -n N -k K -l L --strict
+
+Note: Enter your constraint parameters in place of N, K and L by following definitions given above for each option. --strict option is not necessary to generate CNF formula. By specifying --strict, every clause contained in a generated formula will be having L literals.
---
+++ RandomFormulaGenerator.groovy
@@ -0,0 +1,143 @@
+package RandomSat
+
+import groovy.transform.Field
+
+@Field int numberOfNonEmptyClauses
+@Field int maxNumberOfVars
+@Field int maxSizeOfClause
+@Field Random random = new Random()
+@Field allFormulas = []
+@Field strictNumOfVars
+@Field List formula = []
+
+
+void gettersFromCommandLine() {
+    numberOfNonEmptyClauses = Integer.parseInt(args[1])
+    maxNumberOfVars = Integer.parseInt(args[3])
+    maxSizeOfClause = Integer.parseInt(args[5])
+    try {
+        strictNumOfVars = args[6]
+    }
+    catch (ArrayIndexOutOfBoundsException e) {
+        println("Procceding to generate without --strict \n")
+    }
+    def inputsFile = new File("inputs.txt")
+    inputsFile << numberOfNonEmptyClauses.toString()+" "+maxNumberOfVars.toString()+" "+maxSizeOfClause.toString()+" "+strictNumOfVars
+}
+gettersFromCommandLine()
+generateFormula()
+
+void generateFormula() {
+    def loopCounter = 1
+    while ({
+        def clause = getOneClause()
+        println(clause)
+        if (!isRedundant(clause)) {
+            formula << clause
+            ++loopCounter
+        }
+        loopCounter <= numberOfNonEmptyClauses
+    }())
+
+    allFormulas << formula
+
+    for (List itr : allFormulas) {
+        println(itr)
+    }
+    if (formula.size() > 0) {
+        writeToFile()
+    }
+}
+
+List getOneClause() {
+    try {
+        randomlyGeneratedNumber = random.nextInt(maxNumberOfVars) + 1
+    }
+    catch (IllegalArgumentException e) {
+        boundError()
+    }
+    def randomlyGeneratedSignedNumber = 0
+    def generatedClause = []
+    def sizeOfClause = 0
+    if (strictNumOfVars == "--strict") {
+        sizeOfClause = maxSizeOfClause
+    } else {
+        try {
+            sizeOfClause = random.nextInt(maxSizeOfClause) + 1 // This will be shadowed by setting strict
+        } catch (IllegalArgumentException e) {
+            boundError()
+        }
+    }
+    while (generatedClause.size() < sizeOfClause) {
+        def randomlyGeneratedNumber = 0
+        try {
+            randomlyGeneratedNumber = random.nextInt(maxNumberOfVars) + 1
+        }
+        catch (IllegalArgumentException e) {
+            boundError()
+        }
+        def signOfVar = random.nextInt(2)
+        switch (signOfVar) {
+            case 0:
+                randomlyGeneratedSignedNumber = randomlyGeneratedNumber
+                break
+            case 1:
+                randomlyGeneratedSignedNumber = -1 * randomlyGeneratedNumber
+                break
+            default:
+                print("Impossible by documentation!!!")
+        }
+        if (!generatedClause.contains(-1 * randomlyGeneratedSignedNumber)) {
+            generatedClause << randomlyGeneratedSignedNumber
+        }
+    }
+    return generatedClause
+}
+
+void boundError() {
+    println("Bound on all inputs must be postiive")
+    numberOfNonEmptyClauses = 0
+    maxSizeOfClause = 0
+}
+
+void writeToFile() {
+    def fileNumber = 1
+    def cnfFile
+    def filename
+    while ({
+        filename = "RandomCnfFormula"+fileNumber+".cnf"
+        cnfFile = new File("tree/"+filename)
+        ++fileNumber
+        cnfFile.exists()
+    }());
+    cnfFile << "c "+filename
+    cnfFile << System.getProperty("line.separator").toString()
+    cnfFile << "p cnf "+numberOfNonEmptyClauses.toString()+" "+maxNumberOfVars.toString()+" "+maxSizeOfClause.toString()
+    for (List oneClauseAtATime : formula){
+        cnfFile << System.getProperty("line.separator").toString()
+        for (int literal : oneClauseAtATime){
+            cnfFile << literal.toString()+" "
+        }
+        cnfFile << "0"
+    }
+}
+
+boolean isRedundant(List clause) {
+    def flag = false
+    redundancy:
+    {
+        for (List itr1 : formula) {
+            if (itr1.size() == clause.size()) {
+                def innerFlag = false//if size is same then check further
+                for (int itr2 : itr1) {
+                    innerFlag = innerFlag && clause.contains(itr2)
+                    if (!innerFlag) break
+                }
+                if (flag == true) {
+                    break redundancy
+                }
+            }
+        }
+    }
+    return flag
+}
